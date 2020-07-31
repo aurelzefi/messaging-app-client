c31f574f7421fa610e9c5ca8885e297210f6bcad
Aurel Zefi <aurelzefi1994@gmail.com>
2020-07-31 17:37:07 +0200

    wip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,25 +6,38 @@
       </div>
 
       <div>
-      
-      </div>
+        <ul v-if="chats.length">
+          <li v-for="chat in chats" :key="chat.id" @click="getChat(chat)">
+            {{ chatUser(chat).name }}
 
-      <div>
-      
-      </div>      
+            {{ chat.created_at }}
+          </li>
+        </ul>
+
+        <div v-else>
+          No chats to display.
+        </div>
+      </div>
     </div>
 
     <div>
-      <div>
-        
+      <div v-if="chat">
+        <span>{{ chatUser(chat).name }}</span>
+
+        <button type="button" @click="openFileBrowser">Add Files</button>
+        <input type="file" ref="files" multiple @change="handleFiles">
       </div>
 
       <div>
-      
+        <ul v-if="messages.length">
+          <li v-for="message in messages" :key="message.id">
+            {{ message.content }}
+          </li>
+        </ul>
       </div>
 
       <div>
-      
+        <input type="text" v-model="form.message">
       </div>  
     </div>
   </div>
@@ -32,8 +45,52 @@
 
 <script>
 export default {
-  mounted() {
-    console.log(this.$bus.user)
+  data() {
+    return {
+      chats: [],
+
+      messages: [],
+
+      chat: null,
+
+      form: {
+        message: ''
+      }
+    }
   },
+
+  mounted() {
+    this.getChats()
+  },
+
+  methods: {
+    getChats() {
+      this.$http.get('/api/chats')
+        .then(response => {
+          this.chats = response.data
+        })
+    },
+
+    getChat(chat) {
+      this.chat = chat
+
+      this.$http.get(`/api/chats/${this.chatUser(chat).id}`)
+        .then(response => {
+          this.messages = response.data
+        })
+    },
+
+    chatUser(chat) {
+      return this.$bus.user.id === chat.sender.id ? chat.receiver : chat.sender;
+    },
+
+    openFileBrowser() {
+
+    },
+
+    handleFiles() {
+
+    }
+  }
 }
 </script>
