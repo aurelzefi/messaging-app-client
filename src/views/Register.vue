85e338dfdf70015fffbc2d09ff9046d75d56a7be
Aurel Zefi <aurelzefi1994@gmail.com>
2020-08-06 15:35:50 +0200

    wip

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,36 +1,43 @@
 <template>
-  <div>
-    <div>
-      <img src="./../assets/logo.png">
-    </div>
-
-    <form @submit.prevent="register">
-      <div>
-        <label for="name">Name</label>
-        <input id="name" type="text" v-model="form.name">
+  <div class="antialiased min-h-screen flex flex-col justify-center items-center bg-gray-200">
+      <div class="h-24 w-24">
+        <img src="./../assets/logo.png">
       </div>
 
-      <div>
-        <label for="email">Email</label>
-        <input id="email" type="email" v-model="form.email">
-      </div>
+      <div class="mt-6 px-6 py-4 bg-white rounded-md" style="width: 24rem;">
+        <form @submit.prevent="register">
+          <div>
+            <label class="block text-sm text-gray-700" for="name">Name</label>
+            <input class="w-full mt-1 px-3 py-2 border border-gray-300 rounded outline-none focus:border-gray-400" :class="{ 'border-red-700': this.errors.name }" id="name" type="text" v-model="form.name">
+            <span class="text-xs text-red-700">{{ this.errors.name }}</span>
+          </div>
 
-      <div>
-        <label for="password">Password</label>
-        <input id="password" type="password" v-model="form.password">
-      </div>
+          <div class="mt-4">
+            <label class="block text-sm text-gray-700" for="email">Email</label>
+            <input class="w-full mt-1 px-3 py-2 border border-gray-300 rounded outline-none focus:border-gray-400" :class="{ 'border-red-700': this.errors.email }" id="email" type="email" v-model="form.email">
+            <span class="text-xs text-red-700">{{ this.errors.email }}</span>
+          </div>
 
-      <div>
-        <label for="password-confirm">Confirm Password</label>
-        <input id="password-confirm" type="password" v-model="form.password_confirmation">
-      </div>
+          <div class="mt-4">
+            <label class="block text-sm text-gray-700" for="password">Password</label>
+            <input class="w-full mt-1 px-3 py-2 border border-gray-300 rounded outline-none focus:border-gray-400" :class="{ 'border-red-700': this.errors.password }" id="password" type="password" v-model="form.password">
+            <span class="text-xs text-red-700">{{ this.errors.password }}</span>
+          </div>
 
-      <div>
-        <button type="submit">Register</button>
+          <div class="mt-4">
+            <label class="block text-sm text-gray-700" for="password-confirm">Confirm Password</label>
+            <input class="w-full mt-1 px-3 py-2 border border-gray-300 rounded outline-none focus:border-gray-400" id="password-confirm" type="password" v-model="form.password_confirmation">
+          </div>
 
-        Already have an account? <router-link to="login">Login</router-link>
+          <div class="flex justify-between items-center mt-4 text-sm">
+            <button class="py-2 px-4 bg-blue-500 rounded text-white border-blue-700 outline-none hover:bg-blue-600 focus:outline-none" type="submit">Register</button>
+            
+            <div class="text-gray-700">
+              Already have an account? <router-link class="px-1 hover:underline" to="login">Login</router-link>
+            </div>
+          </div>
+        </form>
       </div>
-    </form>
   </div>
 </template>
 
@@ -57,6 +64,8 @@ export default {
      * Register the user.
      */
     register() {
+      this.errors = {}
+
       this.$http.post('/api/user', this.form)
         .then(response => {
           this.handleAfterAuth(response.data.token)
